2186c79043c152868af3a5d3980e3486e05d4aa4
Artem <iriqtu@gmail.com>
2025-08-03 17:25:50 +0300

    Initial Commit

---
+++ Hohma/HohmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HohmaApp.swift
+//  Hohma
+//
+//  Created by Artem Vydro on 03.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct HohmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
